2233e6a6ef89ae20f8a9158cd4ab99154a2d5fc5
chaitanya-devare15 <chaitanyadevare93@gmail.com>
2025-05-29 08:45:38 +0530

    [ADDED]:chaitanya - C Program for addition of 2 numbers

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("\nENTER VALUE IS a:");
+    scanf("%d",&a);
+
+    printf("\nENTER VALUE IS b:");
+    scanf("%d",&b);
+    printf("sum is%d",a+b);
+    return 0;
+}
